1bfeb47b48006e4ba8dcec1c21a3c99d4790aefe
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2017-11-04 14:20:07 -0400

    refactor: dry up acceptance test format

--- __tests__/acceptance/scenarios.spec.js
+++ __tests__/acceptance/scenarios.spec.js
@@ -1,23 +1,30 @@
 import { parseInput } from '../../src/time-range-parser';
 
-describe('Scenario 1: Basic Subtraction', () => {
+const testOutput = (input, expectedOutput) => {
     it('should subtract the expected value', () => {
-        const input = '(9:00-10:00) "minus" (9:00-9:30)';
-        const expectedOutput = '(9:30-10:00)';
-
         const actualOutput = parseInput(input);
 
         expect(actualOutput).toEqual(expectedOutput);
     });
+};
+
+describe('Scenario 1: Subtraction with Matching Starts', () => {
+    testOutput(
+        '(9:00-10:00) "minus" (9:00-9:30)',
+        '(9:30-10:00)'
+    );
 });
 
 describe('Scenario 2: Empty Output', () => {
-    it('should subtract the expected value', () => {
-        const input = '(9:00-10:00) "minus" (9:00-10:00)';
-        const expectedOutput = '()';
+    testOutput(
+        '(9:00-10:00) "minus" (9:00-10:00)',
+        '()'
+    );
+});
 
-        const actualOutput = parseInput(input);
-
-        expect(actualOutput).toEqual(expectedOutput);
-    });
+describe('Scenario 3: Subtraction with Non-Overlapping Ranges', () => {
+    testOutput(
+        '(9:00-9:30) "minus" (9:30-15:00)',
+        '(9:00-9:30)'
+    );
 });

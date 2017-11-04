4b04dc85b912bf2f6294a5a0f901735f5b9c8a6b
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2017-11-04 14:14:17 -0400

    fix: correct the acceptance test inputs

--- __tests__/acceptance/scenarios.spec.js
+++ __tests__/acceptance/scenarios.spec.js
@@ -2,8 +2,19 @@ import { parseInput } from '../../src/time-range-parser';
 
 describe('Scenario 1: Basic Subtraction', () => {
     it('should subtract the expected value', () => {
-        const input = '(09:00-10:00) "minus" (09:00-09:30)';
-        const expectedOutput = '(09:30-10:00)';
+        const input = '(9:00-10:00) "minus" (9:00-9:30)';
+        const expectedOutput = '(9:30-10:00)';
+
+        const actualOutput = parseInput(input);
+
+        expect(actualOutput).toEqual(expectedOutput);
+    });
+});
+
+describe('Scenario 2: Empty Output', () => {
+    it('should subtract the expected value', () => {
+        const input = '(9:00-10:00) "minus" (9:00-10:00)';
+        const expectedOutput = '()';
 
         const actualOutput = parseInput(input);
 

7e2e47e74f59596de403141a0af9d5b4f8478c6a
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2017-11-05 01:07:19 -0500

    refactor: address complexity in subtractor

--- src/time-range-subtractor.js
+++ src/time-range-subtractor.js
@@ -25,6 +25,42 @@ const getMomentRange = (range) => ({
     start: getMomentTime(range.start),
     end: getMomentTime(range.end)
 });
+
+const processBeforeOrEqualsBase = (candidatesForInsertion, candidate, sub, subTime, i) => {
+    if (subEndsWithinBase(candidate.baseTime, subTime)) {
+        candidate.base.start = getTime(sub.end.hours, sub.end.minutes);
+        candidate.baseTime.start = getMomentTime({
+            hours: sub.end.hours,
+            minutes: sub.end.minutes
+        });
+    } else if (subEndsAfterOrEqualsBase(candidate.baseTime, subTime)) {
+        candidatesForInsertion.splice(i, 1);
+    }
+};
+
+const processSubAfterBase = (candidatesForInsertion, candidate, sub, subTime) => {
+    if (subEndsBeforeBase(candidate.baseTime, subTime)) {
+        const newCandidateBase = {
+            start: getTime(sub.end.hours, sub.end.minutes),
+            end: getTime(candidate.base.end.hours, candidate.base.end.minutes)
+        };
+        const newCandidateBaseTime = getMomentRange(newCandidateBase);
+
+        candidatesForInsertion.push({
+            base: newCandidateBase,
+            baseTime: newCandidateBaseTime
+        });
+    }
+
+    if (subStartsBeforeBaseEnds(candidate.baseTime, subTime)) {
+        candidate.base.end = getTime(sub.start.hours, sub.start.minutes);
+        candidate.baseTime.end = getMomentTime({
+            hours: sub.start.hours,
+            minutes: sub.start.minutes
+        });
+    }
+};
+
 export function subtractRanges(baseRanges, subtractiveRanges) {
     const candidatesForInsertion = baseRanges.map((base) => {
         const baseTime = getMomentRange(base);
@@ -42,36 +78,9 @@ export function subtractRanges(baseRanges, subtractiveRanges) {
             const subTime = getMomentRange(sub);
 
             if (subStartsBeforeOrEqualsBase(candidate.baseTime, subTime)) {
-                if (subEndsWithinBase(candidate.baseTime, subTime)) {
-                    candidate.base.start = getTime(sub.end.hours, sub.end.minutes);
-                    candidate.baseTime.start = getMomentTime({
-                        hours: sub.end.hours,
-                        minutes: sub.end.minutes
-                    });
-                } else if (subEndsAfterOrEqualsBase(candidate.baseTime, subTime)) {
-                    candidatesForInsertion.splice(i, 1);
-                }
+                processBeforeOrEqualsBase(candidatesForInsertion, candidate, sub, subTime, i);
             } else {
-                if (subEndsBeforeBase(candidate.baseTime, subTime)) {
-                    const newCandidateBase = {
-                        start: getTime(sub.end.hours, sub.end.minutes),
-                        end: getTime(candidate.base.end.hours, candidate.base.end.minutes)
-                    };
-                    const newCandidateBaseTime = getMomentRange(newCandidateBase);
-
-                    candidatesForInsertion.push({
-                        base: newCandidateBase,
-                        baseTime: newCandidateBaseTime
-                    });
-                }
-
-                if (subStartsBeforeBaseEnds(candidate.baseTime, subTime)) {
-                    candidate.base.end = getTime(sub.start.hours, sub.start.minutes);
-                    candidate.baseTime.end = getMomentTime({
-                        hours: sub.start.hours,
-                        minutes: sub.start.minutes
-                    });
-                }
+                processSubAfterBase(candidatesForInsertion, candidate, sub, subTime);
             }
         });
     }

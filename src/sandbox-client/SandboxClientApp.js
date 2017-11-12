73fb7e6e9270c0637cb546cf2b5c211ab3ee915f
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2017-11-12 02:16:38 -0500

    style: slight tweaks to ui

--- src/sandbox-client/SandboxClientApp.js
+++ src/sandbox-client/SandboxClientApp.js
@@ -14,13 +14,17 @@ function SandboxApp() {
             <main className='sandbox-client-app'>
                 <h1>{'Time Range Parser'}</h1>
                 <section className='instructions'>
-                    <p>{'This tool allows you to subtract time ranges from a set of time ranges. Simply enter dates in either of the first two inputs. You may only use 24-hour formatting. Follow this input format. For multiple ranges, separate your ranges with a comma.'}
-                    </p>
-                    <p>
-                        <code>{'hh:mm-hh:mm'}</code>
-                    </p>
+                    <p>{'This tool allows you to subtract time ranges from a set of time ranges. Simply enter dates in either of the first two inputs. '}</p>
+                    <ul>
+                        <li>{'Follow this input format:'} <code>{'hh:mm-hh:mm'}</code></li>
+                        <li>{'You may only use 24-hour formatting.'} <code>{'1:00-21:00'}</code> {'is equal to "1am to 9pm"'}</li>
+                        <li>{'For multiple ranges, separate your ranges with a comma.'} <code>{'9:30-13:00, 12:00-18:00'}</code></li>
+                    </ul>
                 </section>
+
                 <SandboxClientConnector/>
+
+                <h2>{'Example Cases'}</h2>
                 <TestExamplesConnector/>
             </main>
         </StoreProvider>
--- src/sandbox-client/TestExamples.js
+++ src/sandbox-client/TestExamples.js
@@ -5,7 +5,7 @@ import { testRanges } from '../state/constants';
 function TestExamples(props) {
     return (
         <section className='test-examples'>
-            <p>{'These were the ranges provided with the challenge to test against.'}</p>
+            <p>{'These were the ranges provided with the challenge to test against. Click Apply to test them out above.'}</p>
             <ul className='examples'>
                 {
                     testRanges.map((item, index) => (
@@ -22,7 +22,7 @@ function TestExamples(props) {
                                 {'Apply'}
                             </button>
                             <code>
-                                {`${item.baseRangeText} "minus" ${item.subtractiveRangeText} = ${item.outputText}`}
+                                {`(${item.baseRangeText}) "minus" (${item.subtractiveRangeText}) = (${item.outputText})`}
                             </code>
                         </li>
                     ))
